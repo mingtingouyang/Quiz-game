b9454ca8f3409317d37b9bee782d9501ad08fd3b
mingtingouyang <36928035+mingtingouyang@users.noreply.github.com>
2019-06-11 16:50:55 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(".welcomePage .start").on('ontouchstart',function () {
+	$(".welcomePage .start").css('box-shadow','0px 6px 10px dimgrey')
+})
+$('.welcomePage .start').on('ontouchend',function () {
+	$(".welcomePage .start").css('box-shadow','none')
+})
